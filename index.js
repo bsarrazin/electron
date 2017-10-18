ef5569b72d4a896e7be0010e5049ce5b5bf9a170
Ben Sarrazin <ben@7swift.io>
2017-10-18 13:16:02 -0400

    Adding windows

--- index.js
+++ index.js
@@ -4,7 +4,27 @@ var app = electron.app;
 
 
 app.on('ready', function() {
-  var appWindow;
-  appWindow = new BrowserWindow();
+  var appWindow, infoWindow;
+  appWindow = new BrowserWindow({
+      show: false
+  });
   appWindow.loadURL('http://raybo.org');
+
+  infoWindow = new BrowserWindow({
+      width: 400,
+      height: 300,
+      show: false,
+      frame: false,
+      transparent: true
+  })
+  infoWindow.loadURL('file://' + __dirname + '/info.html')
+
+  appWindow.once('ready-to-show', function() {
+      appWindow.show()
+      setTimeout(function() {
+        infoWindow.show()
+        setTimeout(function() { infoWindow.hide() }, 3000);
+      }, 1000);
+  })
+
 });
