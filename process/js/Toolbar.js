43dd9972acd9067c6c9518a4243f92e724666e62
Ben Sarrazin <ben@7swift.io>
2017-10-24 11:54:47 -0400

    Adding modal form

--- process/js/Toolbar.js
+++ process/js/Toolbar.js
@@ -1,19 +1,26 @@
 var React = require('react');
 
 var Toolbar = React.createClass({
+  createAppointment: function() {
+    this.props.handleToggle();
+  },
   toggleAbout: function() {
     this.props.handleAbout();
   },
   render: function() {
     return(
       <div className="toolbar">
+        <div className="toolbar-item" onClick={this.createAppointment}>
+          <span className="toolbar-item-button glyphicon glyphicon-plus-sign"></span>
+          <span className="toolbar-item-text">Add AddAppointment</span>
+        </div>
         <div className="toolbar-item" onClick={this.toggleAbout}>
           <span className="toolbar-item-button glyphicon glyphicon-question-sign"></span>
           <span className="toolbar-item-text">About this app</span>
         </div>
       </div>
-    ) //return
-  } //render
-}); //Toolbar
+    )
+  }
+});
 
 module.exports = Toolbar;
--- process/js/render.js
+++ process/js/render.js
@@ -11,21 +11,28 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var AptList = require('./AptList');
 var Toolbar = require('./Toolbar');
+var AddAppointment = require('./AddAppointment')
 
 var MainInterface = React.createClass({
   getInitialState: function() {
     return {
+      aptBodyVisible: false,
       myAppointments: loadApts
-    }//return
-  }, //getInitialState
+    }
+  },
 
   componentDidUpdate: function() {
     fs.writeFile(dataLocation, JSON.stringify(this.state.myAppointments), 'utf8', function(err) {
       if (err) {
         console.log(err);
       }
-    });//writeFile
-  }, //componentDidUpdate
+    });
+  },
+
+  toggleAptDisplay: function() {
+    var tempVisibility = !this.state.aptBodyVisible
+    this.setState({aptBodyVisible: tempVisibility})
+  },
 
   showAbout:function() {
     ipc.sendSync('openInfoWindow');
@@ -36,27 +43,32 @@ var MainInterface = React.createClass({
     var newApts = _.without(allApts, item);
     this.setState({
       myAppointments: newApts
-    }); //setState
-  }, //deleteMessage
+    });
+  },
 
   render: function() {
     var myAppointments = this.state.myAppointments;
 
+    if (this.state.aptBodyVisible === true) {
+      $('#addAppointment').modal('show')
+    } else {
+      $('#addAppointment').modal('hide')
+    }
+
     myAppointments=myAppointments.map(function(item, index) {
       return(
         <AptList key = {index}
-          singleItem = {item}
-          whichItem =  {item}
-          onDelete = {this.deleteMessage}
+                 singleItem = {item}
+                 whichItem =  {item}
+                 onDelete = {this.deleteMessage}
         />
-      ) // return
-    }.bind(this)); //Appointments.map
+      )
+    }.bind(this));
     return(
       <div className="application">
         <div className="interface">
-          <Toolbar
-            handleAbout = {this.showAbout}
-          />
+          <Toolbar handleToggle = {this.toggleAptDisplay} handleAbout = {this.showAbout} />
+          <AddAppointment handleToggle = {this.toggleAptDisplay} />
           <div className="container">
            <div className="row">
              <div className="appointments col-sm-12">
@@ -68,10 +80,10 @@ var MainInterface = React.createClass({
         </div>{/* interface */}
       </div>
     );
-  } //render
-});//MainInterface
+  }
+});
 
 ReactDOM.render(
   <MainInterface />,
   document.getElementById('petAppointments')
-); //render
+);
